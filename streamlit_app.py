5bbee3c1b2e52cec6aa5eea2db8299c4e273e40c
slawomir-nowaczyk <51878354+slawomir-nowaczyk@users.noreply.github.com>
2023-11-11 10:03:38 +0100

    next version

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+num_points = st.slider("Number of points in my spiral", 1, 10000, 1100)
+num_turns = st.slider("Number of turns in my spiral", 1, 300, 31)
 
 indices = np.linspace(0, 1, num_points)
 theta = 2 * np.pi * num_turns * indices
